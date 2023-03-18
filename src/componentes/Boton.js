3814d9e22430b229395cc92d073ef9ababcce7c8
Mario Sanchez <marsanch@gmail.com>
2023-03-18 17:10:17 -0500

    Agregada la funcionalidad

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -8,10 +8,31 @@ function Boton(props) {
   };
 
   return (
-      <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`.trimEnd()}>
-        {props.children}
-      </div>
+    <div 
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)}
+      >
+      {props.children}
+    </div>
   )
+
+  //Otra Forma para darle color diferente a los numeros y operadores
+
+  /* if (esOperador(props.children)) {
+    <div 
+      className='boton-contenedor operador'
+      onClick={() => props.manejarClic(props.children)}
+      >
+      {props.children}
+    </div>
+  } else {
+    <div 
+      className='boton-contenedor'
+      onClick={() => props.manejarClic(props.children)}
+      >
+      {props.children}
+    </div>  
+  } */
 }
 
 export default Boton;
--- src/componentes/BotonClear.js
+++ src/componentes/BotonClear.js
@@ -2,7 +2,10 @@ import React from 'react';
 import '../hojas-de-estilo/BotonClear.css';
 
 const BotonClear = (props) => (
-  <div className='boton-clear'>
+  <div 
+    className='boton-clear'
+    onClick={props.manejarClear}
+    >
     {props.children}
   </div>
 )
